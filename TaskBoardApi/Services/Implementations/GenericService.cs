81b1bb77161d9cf209fb309f9f92f60ffdfce8e2
Seyfi ISayev <isayevseyfi3277@gmail.com>
2025-11-14 06:24:44 +0400

    feat: Added IGenericService and implemented with GenericService

---
+++ TaskBoardApi/Services/Implementations/GenericService.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using TaskBoardApi.Data;
+using TaskBoardApi.Services.Interfaces;
+
+namespace TaskBoardApi.Services.Implementations
+{
+    public class GenericService<TDto, TEntity> : IGenericService<TDto>
+        where TEntity : class
+    {
+        private readonly TaskBoardDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericService(TaskBoardDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+            _dbSet = context.Set<TEntity>();
+        }
+
+
+        public async Task<List<TDto>> GetAllAsync()
+        {
+            return await _dbSet
+                .ProjectTo<TDto>(_mapper.ConfigurationProvider)
+                .ToListAsync();
+        }
+
+        public async Task<TDto> GetByIdAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+
+            if(entity == null)
+            {
+                return default;
+            }
+
+            return _mapper.Map<TDto>(entity);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+
+            if(entity == null)
+            {
+                return false;
+            }
+
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
---
+++ TaskBoardApi/Services/Interfaces/IGenericService.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskBoardApi.Services.Interfaces
+{
+    public interface IGenericService<TDto>
+    {
+        Task<List<TDto>> GetAllAsync();
+        Task<TDto> GetByIdAsync(int id);
+        Task<bool> DeleteAsync(int id);
+
+        // As our post and put methods for User and Task are completely different we don't create Generic Post or Generic Put -- because of One-To-Many relation
+    }
+}
